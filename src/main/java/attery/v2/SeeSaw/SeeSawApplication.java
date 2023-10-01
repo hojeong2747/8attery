e69e6e5d8ef7c2899148d50e02c207319263378b
hojeong2747 <hojeong2747@gmail.com>
2023-10-01 10:44:35 +0900

    feat: Spring Boot Application 추가

---
+++ src/main/java/attery/v2/SeeSaw/SeeSawApplication.java
@@ -0,0 +1,13 @@
+package attery.v2.SeeSaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeSawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeSawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/attery/v2/SeeSaw/SeeSawApplicationTests.java
@@ -0,0 +1,13 @@
+package attery.v2.SeeSaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeSawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
