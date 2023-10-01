35fd222b3e3abb8b80ff28232732d35c2c91d10d
hojeong2747 <hojeong2747@gmail.com>
2023-10-01 14:16:12 +0900

    feat: health check용 api 추가

---
+++ src/main/java/attery/v2/SeeSaw/controller/HealthController.java
@@ -0,0 +1,13 @@
+package attery.v2.SeeSaw.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "healthy";
+    }
+}
